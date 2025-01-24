7786c18f28bf29b7d424581bb0393d1eab2085ab
Jakub Konečný <jakub.konecny2@centrum.cz>
2025-01-24 20:23:29 +0100

    rewritten tests for simple null cache test the whole process just like for simple memory cache

--- tests/Simple/NullCacheTest.php
+++ tests/Simple/NullCacheTest.php
@@ -9,78 +9,105 @@ use MyTester\TestCase;
 #[TestSuite("NullCache")]
 final class NullCacheTest extends TestCase
 {
-    public function testGet(): void
+    public function testSingleKeyProcess(): void
+    {
+        $key = "abc";
+        $value = "def";
+        $default = "default";
+        $cache = new NullCache();
+
+        $this->assertFalse($cache->has($key));
+        $this->assertSame($default, $cache->get($key, $default));
+
+        $cache->set($key, $value, -1);
+        $this->assertFalse($cache->has($key));
+        $this->assertSame($default, $cache->get($key, $default));
+
+        $cache->set($key, $value, 30);
+        $this->assertFalse($cache->has($key));
+        $this->assertSame($default, $cache->get($key, $default));
+
+        $cache->delete($key);
+        $this->assertFalse($cache->has($key));
+        $this->assertSame($default, $cache->get($key, $default));
+    }
+
+    public function testMultiKeysProcess(): void
+    {
+        $key1 = "one";
+        $value1 = "abc";
+        $default = "default";
+        $key2 = "two";
+        $value2 = "def";
+        $cache = new NullCache();
+
+        $this->assertFalse($cache->has($key1));
+        $this->assertFalse($cache->has($key2));
+        $this->assertSame(
+            [$key1 => $default, $key2 => $default, ],
+            $cache->getMultiple([$key1, $key2, ], $default)
+        );
+
+        $cache->setMultiple([$key1 => $value1, $key2 => $value2, ], -1);
+        $this->assertFalse($cache->has($key2));
+        $this->assertSame(
+            [$key1 => $default, $key2 => $default, ],
+            $cache->getMultiple([$key1, $key2, ], $default)
+        );
+
+        $cache->setMultiple([$key1 => $value1, $key2 => $value2, ], 30);
+        $this->assertFalse($cache->has($key1));
+        $this->assertFalse($cache->has($key2));
+        $this->assertSame(
+            [$key1 => $default, $key2 => $default, ],
+            $cache->getMultiple([$key1, $key2, ], $default)
+        );
+
+        $cache->deleteMultiple([$key1, $key2, ]);
+        $this->assertFalse($cache->has($key1));
+        $this->assertFalse($cache->has($key2));
+        $this->assertSame(
+            [$key1 => $default, $key2 => $default, ],
+            $cache->getMultiple([$key1, $key2, ], $default)
+        );
+
+        $cache->setMultiple([$key1 => $value1, $key2 => $value2, ], 30);
+        $this->assertTrue($cache->clear());
+        $this->assertFalse($cache->has($key1));
+        $this->assertFalse($cache->has($key2));
+        $this->assertSame(
+            [$key1 => $default, $key2 => $default, ],
+            $cache->getMultiple([$key1, $key2, ], $default)
+        );
+    }
+
+    public function testExceptions(): void
     {
         $cache = new NullCache();
-        $this->assertSame(null, $cache->get("abc"));
-        $this->assertSame("def", $cache->get("abc", "def"));
         $this->assertThrowsException(function () use ($cache) {
             $cache->get("{");
         }, InvalidKeyException::class);
-    }
 
-    public function testSet(): void
-    {
-        $cache = new NullCache();
-        $this->assertTrue($cache->set("abc", null));
-        $this->assertTrue($cache->set("abc", null));
         $this->assertThrowsException(function () use ($cache) {
             $cache->set("{", "abc");
         }, InvalidKeyException::class);
-    }
 
-    public function testDelete(): void
-    {
-        $cache = new NullCache();
-        $this->assertTrue($cache->delete("abc"));
-        $this->assertTrue($cache->delete("abc"));
         $this->assertThrowsException(function () use ($cache) {
             $cache->delete("{");
         }, InvalidKeyException::class);
-    }
 
-    public function testClear(): void
-    {
-        $cache = new NullCache();
-        $this->assertTrue($cache->clear());
-        $this->assertTrue($cache->clear());
-    }
-
-    public function testGetMultiple(): void
-    {
-        $cache = new NullCache();
-        $this->assertSame(
-            ["one" => "abc", "two" => "abc", ],
-            $cache->getMultiple(["one", "two", ], "abc")
-        );
         $this->assertThrowsException(function () use ($cache) {
             $cache->getMultiple(["one", "{"]);
         }, InvalidKeyException::class);
-    }
 
-    public function testSetMultiple(): void
-    {
-        $cache = new NullCache();
-        $this->assertTrue($cache->setMultiple(["one" => "abc", "two" => "def", ]));
         $this->assertThrowsException(function () use ($cache) {
             $cache->setMultiple(["{" => "abc", ]);
         }, InvalidKeyException::class);
-    }
 
-    public function testDeleteMultiple(): void
-    {
-        $cache = new NullCache();
-        $this->assertTrue($cache->deleteMultiple(["one", "two", ]));
-        $this->assertTrue($cache->deleteMultiple(["one", "two", ]));
         $this->assertThrowsException(function () use ($cache) {
             $cache->deleteMultiple(["one", "{"]);
         }, InvalidKeyException::class);
-    }
 
-    public function testHas(): void
-    {
-        $cache = new NullCache();
-        $this->assertFalse($cache->has("abc"));
         $this->assertThrowsException(function () use ($cache) {
             $cache->has("{");
         }, InvalidKeyException::class);
